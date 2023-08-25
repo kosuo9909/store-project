ba87e3d0a85cc7889af2ca1427bb898bc9217c4d
Kostadin Stanev <stanebvns@gmail.com>
2023-08-25 23:50:20 +0300

    basic card structure

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -8,11 +8,17 @@ import Typography from '@mui/material/Typography';
 import SingleListing from './SingleListing';
 
 const storage = {
-  brand: 'Ford',
+  brand: 'Ford Escort',
   price: 1444,
   year: 1992,
   bhp: 110,
   miles: 114556,
+  fuel: 'diesel',
+  city: 'Veliko Tarnovo',
+  country: 'Bulgaria',
+  datePosted: 'Today, 15:53 PM',
+  description:
+    'I have had this car for 17 years now and I cannot stand it anymore. Please someone buy it. Thanks!',
 };
 
 export default function Shop() {
--- src/components/SingleListing.tsx
+++ src/components/SingleListing.tsx
@@ -7,17 +7,22 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 interface DetailsType {
+  datePosted: string;
   brand: string;
   price: number;
   year: number;
   bhp: number;
   miles: number;
+  fuel: string;
+  description: string;
+  city: string;
+  country: string;
 }
 
 export default function SingleListing({ details }: { details: DetailsType }) {
   return (
     <div className='test'>
-      <Box sx={{ minWidth: 275 }}>
+      <Box sx={{ minWidth: 275, maxWidth: 350 }}>
         <Card variant='outlined'>
           <CardContent>
             <Typography
@@ -25,7 +30,7 @@ export default function SingleListing({ details }: { details: DetailsType }) {
               color='text.secondary'
               gutterBottom
             >
-              For sale
+              {details.datePosted}
             </Typography>
             <Typography variant='h5' component='div'>
               {details.brand}
@@ -33,11 +38,15 @@ export default function SingleListing({ details }: { details: DetailsType }) {
             <Typography sx={{ mb: 1.5 }} color='text.secondary'>
               {details.price} USD
             </Typography>
-            <Typography variant='body2'>
-              {details.year} production year
+            <Typography variant='body2' sx={{ marginBottom: 1 }}>
+              {details.year}, {details.fuel}, {details.miles} miles
             </Typography>
-            <Typography variant='body2'>{details.miles} miles</Typography>
-            <Typography variant='body2'>{details.bhp} horsepower</Typography>
+            <Typography variant='body2' color='text.secondary'>
+              {details.description}
+            </Typography>
+            <Typography variant='body2' sx={{ marginTop: 1 }}>
+              {details.city}, {details.country}
+            </Typography>
           </CardContent>
           <CardActions>
             <Button size='small'>Visit listing</Button>
