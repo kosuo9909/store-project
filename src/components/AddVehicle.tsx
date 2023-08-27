27a7a0b4223fc53d392e87ba87f0fd3de4a76e87
Kostadin Stanev <stanebvns@gmail.com>
2023-08-27 12:36:17 +0300

    basic AddVehicle structure

--- src/components/AddVehicle.tsx
+++ src/components/AddVehicle.tsx
@@ -1,54 +1,43 @@
-import { alpha, styled } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import './AddVehicle.scss';
+import React, { useState } from 'react';
+import { ICar } from './interfaces/interfaces';
 
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  'label + &': {
-    marginTop: theme.spacing(3),
-  },
-  '& .MuiInputBase-input': {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#1A2027',
-    border: '1px solid',
-    borderColor: theme.palette.mode === 'light' ? '#E0E3E7' : '#2D3843',
-    fontSize: 16,
-    width: 'auto',
-    padding: '10px 12px',
-    transition: theme.transitions.create([
-      'border-color',
-      'background-color',
-      'box-shadow',
-    ]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}));
+const textFields = [
+  ['make', 'Make'],
+  ['model', 'Model'],
+  ['year', 'Year'],
+  ['mileage', 'Mileage'],
+  ['fuel', 'Fuel'],
+  ['bhp', 'Horsepower'],
+  ['city', 'City'],
+  ['country', 'Country'],
+  ['price', 'Price'],
+  ['description', 'Description'],
+];
 
-export default function AddVehicle() {
+const AddVehicle = () => {
+  const [formData, setFormData] = useState<Partial<ICar>>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData);
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setFormData({});
+  };
+
   return (
     <main>
       <h2>Add a vehicle</h2>
@@ -63,94 +52,51 @@ export default function AddVehicle() {
           gap: 3,
         }}
       >
-        <TextField
-          // error
-
-          id='make'
-          type='input'
-          label='Make'
-          placeholder='Make'
-        />
-        <TextField
-          // error
-          id='model'
-          type='input'
-          label='Model'
-          placeholder='Model'
-        />
-        <TextField
-          // error
-          multiline
-          id='year'
-          type='input'
-          label='Year'
-          placeholder='Year'
-        />
-        <TextField
-          // error
-          multiline
-          id='mileage'
-          type='input'
-          label='Mileage'
-          placeholder='Mileage'
-        />
-        <TextField
-          // error
-          multiline
-          id='fuel'
-          type='input'
-          label='Fuel'
-          placeholder='Fuel'
-        />
-        <TextField
-          // error
-          multiline
-          id='bhop'
-          type='input'
-          label='Horsepower'
-          placeholder='Horsepower'
-        />
-        <TextField
-          // error
-          multiline
-          id='city'
-          type='input'
-          label='City'
-          placeholder='City'
-        />
-        <TextField
-          // error
-          multiline
-          id='country'
-          type='input'
-          label='Country'
-          placeholder='Country'
-        />
-        <TextField
-          // error
-          multiline
-          id='price'
-          type='input'
-          label='Price'
-          placeholder='Price'
-        />
-        <TextField
-          // error
-          multiline
-          id='description'
-          type='input'
-          label='Description'
-          placeholder='Brief description'
-        />
+        {textFields.map(([name, label]) =>
+          name === 'description' ? (
+            <TextField
+              key={name}
+              id={name}
+              name={name}
+              type='input'
+              label={label}
+              value={
+                formData[name as keyof ICar] ? formData[name as keyof ICar] : ''
+              }
+              placeholder={label}
+              onChange={handleChange}
+              multiline
+            />
+          ) : (
+            <TextField
+              key={name}
+              id={name}
+              name={name}
+              type='input'
+              label={label}
+              value={
+                formData[name as keyof ICar] ? formData[name as keyof ICar] : ''
+              }
+              placeholder={label}
+              onChange={handleChange}
+            />
+          )
+        )}
       </Box>
       <Stack direction='row' spacing={2}>
         <Button variant='outlined' startIcon={<DeleteIcon />}>
           Clear
         </Button>
-        <Button variant='contained' endIcon={<SendIcon />}>
+        <Button
+          variant='contained'
+          onClick={handleSubmit}
+          endIcon={<SendIcon />}
+        >
           Submit
         </Button>
       </Stack>
     </main>
   );
-}
+};
+
+export default AddVehicle;
