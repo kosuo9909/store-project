8be8e247ca1d1e89dc4cc230211b975f263047c0
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-09-07 11:43:54 +0300

    car reducer code cleanup

--- src/redux/reducers/carsReducer.ts
+++ src/redux/reducers/carsReducer.ts
@@ -1,71 +1,15 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { ICar } from '../../interfaces/interfaces';
-import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 interface CarState {
   value: ICar[];
   selectedCar?: ICar;
 }
 
-interface EditCarPayload {
-  id: string;
-  updatedCar: Partial<ICar>;
-}
-
 interface EditCarArgs {
   updatedCar: Partial<ICar>;
   carID: string;
 }
-
-const initialState: CarState = {
-  value: [],
-  selectedCar: undefined,
-};
-
-export const carSlice = createSlice({
-  name: 'cars',
-  initialState,
-  reducers: {
-    addCar: (state, action: PayloadAction<ICar>) => {
-      const myID = uuidv4();
-      const carWithDateAndID = {
-        ...action.payload,
-        datePosted: new Date().toISOString(),
-        id: myID,
-      };
-      state.value.push(carWithDateAndID);
-    },
-    removeCar: (state, action: PayloadAction<string>) => {
-      state.value = state.value.filter((car) => car.id !== action.payload);
-    },
-    selectCar: (state, action: PayloadAction<string>) => {
-      state.selectedCar = state.value.find((car) => car.id === action.payload);
-    },
-    editCar: (state, action: PayloadAction<EditCarPayload>) => {
-      const { id, updatedCar } = action.payload;
-      const carIndex = state.value.findIndex((item) => item.id === id);
-
-      if (carIndex !== -1) {
-        state.value[carIndex] = { ...state.value[carIndex], ...updatedCar };
-      }
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(postCars.fulfilled, (state, action) => {
-        console.log('This would do something once postCars is intercepted');
-      })
-      .addCase(fetchCars.fulfilled, (state, action) => {
-        state.value = action.payload;
-      })
-      .addCase(removeCarAPI.fulfilled, (state, action) => {
-        console.log('This would do something once removeCarAPI is intercepted');
-      })
-      .addCase(editCars.fulfilled, (state, action) => {
-        console.log('This would do something once editCars is intercepted');
-      });
-  },
-});
 
 export const fetchCars = createAsyncThunk('cars/fetchCars', async () => {
   try {
@@ -120,5 +64,35 @@ export const postCars = createAsyncThunk(
   },
 );
 
-export const { addCar, removeCar, editCar, selectCar } = carSlice.actions;
+const initialState: CarState = {
+  value: [],
+  selectedCar: undefined,
+};
+
+export const carSlice = createSlice({
+  name: 'cars',
+  initialState,
+  reducers: {
+    selectCar: (state, action: PayloadAction<string>) => {
+      state.selectedCar = state.value.find((car) => car.id === action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(postCars.fulfilled, (state, action) => {
+        console.log('This would do something once postCars is intercepted');
+      })
+      .addCase(fetchCars.fulfilled, (state, action) => {
+        state.value = action.payload;
+      })
+      .addCase(removeCarAPI.fulfilled, (state, action) => {
+        console.log('This would do something once removeCarAPI is intercepted');
+      })
+      .addCase(editCars.fulfilled, (state, action) => {
+        console.log('This would do something once editCars is intercepted');
+      });
+  },
+});
+
+export const { selectCar } = carSlice.actions;
 export default carSlice.reducer;
